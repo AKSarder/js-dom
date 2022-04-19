a82dc52e279cb0caacf66370dead414871d6703f
AKSarder <63703796+AKSarder@users.noreply.github.com>
2022-04-19 15:48:13 +0600

    done

---
+++ js/custom.js
@@ -0,0 +1,15 @@
+// console.log("Hello Custom Js");
+
+
+// fibonacci series
+const fibo = [0, 1];
+// let firstNum = 0;
+// let secondNum = 1;
+let temp = 0;
+console.log(firstNum);
+for (let i = 2; i <= 10; i++) {
+    temp = firstNum + secondNum;
+    firstNum = secondNum;
+    secondNum = temp;
+    console.log(temp);
+}
---
+++ main.js
@@ -0,0 +1,5 @@
+// // console.log("external JS");
+// console.log(document.URL);
+// console.log(document.head);
+// console.log(typeof (document.head))
+// console.log(document.title = "play with dom");
